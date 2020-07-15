d96ac4aa1527d345022320aa74789ff2d0fdb415
eemreerdemir <emre.erdemir@outlook.com.tr>
2020-07-15 19:18:56 +0300

    Update Members.js

--- Members.js
+++ Members.js
@@ -1,5 +1,5 @@
 const members =[  
-    {"name":"ok", "email":"shyamjaiswal@gmail.com"},  
+    {"name":"olduuuu", "email":"shyamjaiswal@gmail.com"},  
     {"name":"Bob", "email":"bob32@gmail.com"},  
     {"name":"Jai", "email":"jai87@gmail.com"}  
 ]
